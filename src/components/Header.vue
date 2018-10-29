f238417055ed5af727997597de7c9141d2fa906f
Emmanuel CB <emmanuelcb@Emmanuels-MacBook-Pro.local>
2018-10-29 11:43:16 +0530

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <ul class="nav nav-pills">
+        <router-link to="/" role="presentation" tag="li" active-class="active" exact><a>Home</a></router-link>
+        <router-link to="/user/1" role="presentation" tag="li" active-class="active"><a>User 1</a></router-link>
+        <router-link to="/user/2" role="presentation" tag="li" active-class="active"><a>User 2</a></router-link>
+    </ul>
+</template>
