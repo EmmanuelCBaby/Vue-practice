3a2513286b76979ef49265f2bba4df1acb9886a5
Emmanuel CB <emmanuelcb@Emmanuels-MacBook-Pro.local>
2018-10-29 11:53:41 +0530

    Added more users

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,5 +3,8 @@
         <router-link to="/" role="presentation" tag="li" active-class="active" exact><a>Home</a></router-link>
         <router-link to="/user/1" role="presentation" tag="li" active-class="active"><a>User 1</a></router-link>
         <router-link to="/user/2" role="presentation" tag="li" active-class="active"><a>User 2</a></router-link>
+        <router-link to="/user/3" role="presentation" tag="li" active-class="active"><a>User 2</a></router-link>
+        <router-link to="/user/4" role="presentation" tag="li" active-class="active"><a>User 2</a></router-link>
+        <router-link to="/user/5" role="presentation" tag="li" active-class="active"><a>User 2</a></router-link>
     </ul>
 </template>
